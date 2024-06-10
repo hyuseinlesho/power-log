49c1101d19c4de5e34b20d309c4ee35c25a038d1
hyuseinlesho <hiuseinleshogithub@gmail.com>
2024-06-10 12:16:17 +0300

    Initial commit

---
+++ src/main/java/com/hyuseinlesho/powerlog/PowerLogApplication.java
@@ -0,0 +1,13 @@
+package com.hyuseinlesho.powerlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PowerLogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hyuseinlesho/powerlog/PowerLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyuseinlesho.powerlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
