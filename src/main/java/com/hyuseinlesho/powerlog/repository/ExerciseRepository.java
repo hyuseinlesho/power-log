57045eaf8f7a2f25596468468fdf1c84ddb643a5
hyuseinlesho <hiuseinleshogithub@gmail.com>
2024-06-11 11:17:49 +0300

    Add repositories for all entities

---
+++ src/main/java/com/hyuseinlesho/powerlog/repository/ExerciseRepository.java
@@ -0,0 +1,9 @@
+package com.hyuseinlesho.powerlog.repository;
+
+import com.hyuseinlesho.powerlog.model.Exercise;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
+}
---
+++ src/main/java/com/hyuseinlesho/powerlog/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.hyuseinlesho.powerlog.repository;
+
+import com.hyuseinlesho.powerlog.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
