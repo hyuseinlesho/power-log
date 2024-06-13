ca67dc684068a91b4063eb45c8a4d458023649cf
hyuseinlesho <hiuseinleshogithub@gmail.com>
2024-06-13 23:31:48 +0300

    Update Contact class validations

--- src/main/java/com/hyuseinlesho/powerlog/model/Contact.java
+++ src/main/java/com/hyuseinlesho/powerlog/model/Contact.java
@@ -1,5 +1,6 @@
 package com.hyuseinlesho.powerlog.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -10,7 +11,13 @@ import lombok.Setter;
 @Entity
 @Table(name = "contacts")
 public class Contact extends BaseEntity {
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private String message;
 }
