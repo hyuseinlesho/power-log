5144b5a0f17295e028e86f70457e4d97c085f557
hyuseinlesho <hiuseinleshogithub@gmail.com>
2024-06-26 14:23:09 +0300

    Add thymeleaf-extras dependency and implement thymeleaf security for layout buttons

--- src/main/java/com/hyuseinlesho/powerlog/controller/HomeController.java
+++ src/main/java/com/hyuseinlesho/powerlog/controller/HomeController.java
@@ -1,12 +1,15 @@
 package com.hyuseinlesho.powerlog.controller;
 
+import com.hyuseinlesho.powerlog.security.SecurityUtil;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping("/home")
-    public String showHomePage() {
+    public String showHomePage(Model model) {
+        model.addAttribute("user", SecurityUtil.getSessionUser());
         return "home";
     }
 
